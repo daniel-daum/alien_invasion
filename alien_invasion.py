7e3e18f519160ac6d0a191c76e57b836187d4891
daniel daum <daniel_daum@outlook.com>
2023-04-24 20:30:23 -0400

    added basic ship

--- alien_invasion.py
+++ alien_invasion.py
@@ -2,6 +2,7 @@ import sys
 
 import pygame
 from settings import Settings
+from ship import Ship
 
 
 class AlienInvasion:
@@ -20,20 +21,53 @@ class AlienInvasion:
         )
         pygame.display.set_caption("Alien Invasion")
 
+        self.ship = Ship(self)
+
     def run_game(self):
         """Start the main loop for the game."""
 
         while True:
             # Watch for keyboard and mouse events.
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    sys.exit()
+            self._check_events()
 
-            self.screen.fill(self.settings.bg_color)
+            self.ship.update()
 
-            # Make the most recently drawn screen available
-            pygame.display.flip()
+            self._update_screen()
+
             self.clock.tick(60)
+
+    def _check_events(self):
+        """Respond to key presses and mouse events."""
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RIGHT:
+                    # Move  the ship to the right
+                    self.ship.moving_right = True
+
+                elif event.key == pygame.K_LEFT:
+                    self.ship.moving_left = True
+
+            elif event.type == pygame.KEYUP:
+                if event.key == pygame.K_RIGHT:
+                    self.ship.moving_right = False
+
+                elif event.key == pygame.K_LEFT:
+                    self.ship.moving_left = False
+
+    def _check_keydown_events(self, event):
+        """Respond to keypresses."""
+
+    def _update_screen(self):
+        """Update images on the screen, and flip to the new screen."""
+
+        self.screen.fill(self.settings.bg_color)
+        self.ship.blitme()
+
+        pygame.display.flip()
 
 
 if __name__ == "__main__":
